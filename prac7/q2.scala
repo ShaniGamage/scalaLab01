323cf18ac655bba41409d698f561e86c4106046b
ShaniGamage <140165819+ShaniGamage@users.noreply.github.com>
2024-08-10 19:32:35 +0530

    Add files via upload

--- prac7/q2.scala
+++ prac7/q2.scala
@@ -1,11 +1,28 @@
-object squreNum{
+import scala.io.StdIn
 
-    def calculateSqure(list:List[Int]):List[Int]={
-        return (list.map(x=>x*x));
+object categorizer{
+
+    println("Enter the integer : ");
+    var input=StdIn.readLine().toInt;
+     
+    var isMultipeThree = (n:Int)=>n%3 == 0;
+    var isMultipeFive = (n:Int)=>n%5 == 0;
+
+     /*var result= input match {
+        case _ if isMultipeThree(input)=> println("")
+     } */
+
+    if(isMultipeThree(input)){
+        println("Multiple of Three");
+    }else if(isMultipeFive(input)){
+        println("Multiple if Five");
+    }else if(isMultipeThree(input) && isMultipeFive(input)){
+        println("Multiple of Both Three and Five");
+    }else{
+        println("Not a Multiple of Three or Five");
     }
 
     def main(args: Array[String]): Unit = {
-        var mylist=List(1,2,3,4,5);
-        println(calculateSqure(mylist));
+        
     }
 }
